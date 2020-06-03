f8ff89b0deb862f49d602424a19bea5f67d58923
byoon97 <byoon5397@gmail.com>
2020-06-03 01:34:38 -0400

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(str) {
+  const newTime = parseInt(str, 10)
+  if (newTime < 12) return "Good Morning"
+  if (newTime > 17) return "Good Evening"
+  return "Good Afternoon"
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
